1e5ae00af817c1e67d4831a0332c42ffcd5de1dc
Liadev <122531419+Liadev-op@users.noreply.github.com>
2026-09-22 01:51:57 -0300

    ui: distinguish Orca Cloud and Snapmaker accounts

--- src/slic3r/GUI/MainFrame.hpp
+++ src/slic3r/GUI/MainFrame.hpp
@@ -99,7 +99,7 @@ class MainFrame : public DPIFrame
 
     wxMenuBar*  m_menubar{ nullptr };
     //wxMenu* publishMenu{ nullptr };
-    wxMenu *    m_calib_menu{nullptr };
+    wxMenu *    m_calib_menu{nullptr};
     bool        enable_multi_machine{ false };
 
 #if 0
